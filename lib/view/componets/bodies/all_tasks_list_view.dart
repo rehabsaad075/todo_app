1904790b6e17291231aef7b547cec3134ae06f23
Rehab Saad <saadrehab075@gmail.com>
2024-06-08 04:08:19 +0300

    done update task screen ui

--- lib/view/componets/bodies/all_tasks_list_view.dart
+++ lib/view/componets/bodies/all_tasks_list_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/view/componets/items/task_item_custom.dart';
+import 'package:todo_app/view/screens/update_task_screen.dart';
+import 'package:todo_app/view_model/functions/navigation_functions.dart';
 
 class AllTasksListView extends StatelessWidget {
   const AllTasksListView({super.key});
@@ -10,7 +12,12 @@ class AllTasksListView extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 8),
         itemBuilder: (context,index){
           return  TaskItemCustom(
-            onTap: () {},
+            onTap: () {
+              navigationPushFunction(
+                  context: context,
+                  screen: const UpdateTaskScreen()
+              );
+            },
           );
         },
         separatorBuilder: (context,index)=> const SizedBox(
