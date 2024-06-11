792959cefbbe74632a68975d011e26b1b4af72fd
Rehab Saad <saadrehab075@gmail.com>
2024-06-11 08:26:17 +0300

    register method done

--- lib/view/screens/auth_screens/register_screen.dart
+++ lib/view/screens/auth_screens/register_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/view/componets/widgets/circular_progress_loading_custom.dart';
 import 'package:todo_app/view/componets/widgets/elvated_button_custom.dart';
 import 'package:todo_app/view/componets/widgets/text_form_field_custom.dart';
 import 'package:todo_app/view/componets/widgets/text_custom.dart';
+import 'package:todo_app/view/screens/todo_screens/todo_home_page_screen.dart';
+import 'package:todo_app/view_model/cubits/auth_cubit/auth_cubit.dart';
+import 'package:todo_app/view_model/utils/functions/navigation_functions.dart';
 import 'package:todo_app/view_model/utils/material/app_colors.dart';
 import 'package:todo_app/view_model/utils/material/app_icons.dart';
 
@@ -10,116 +15,147 @@ class RegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Directionality(
-      textDirection: TextDirection.rtl,
-      child: Scaffold(
-        body: Form(
-          key: GlobalKey<FormState>(),
-          child: ListView(
-            physics: const BouncingScrollPhysics(),
-            padding: const EdgeInsets.only(
-                top: 150,
-                right: 18,
-                left: 18
-            ),
-            children:  [
-              const Opacity(
-                opacity: 0.6,
-                child: TextCustom(
-                  text: 'todo_app',
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 15,),
-              const TextCustom(
-                text: 'إنشاء حساب',
-                fontWeight: FontWeight.bold,
-              ),
-              const SizedBox(height: 30,),
-              TextFormFieldCustom(
-                labelText: 'اسم المستخدم',
-                keyboardType: TextInputType.text,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'اسم المستخدم مطلوب';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10,),
-              TextFormFieldCustom(
-                labelText: 'البريد الالكترنى',
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'البريد الالكترونى مطلوب';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10,),
-              TextFormFieldCustom(
-                labelText: ' كلمة المرور',
-                keyboardType: TextInputType.text,
-                icon: AppIcons.visibilityIcon,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'كلمة المرور مطلوبة';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10,),
-              TextFormFieldCustom(
-                labelText: ' تاكيد كلمة المرور',
-                keyboardType: TextInputType.text,
-                textInputAction: TextInputAction.done,
-                icon: AppIcons.visibilityIcon,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'كلمة المرور مطلوبة';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 30,),
-              ElevatedButtonCustom(
-                onPressed: () {
-
-                },
-                child: const TextCustom(
-                  text: 'إنشاء حساب',
-                  color: AppColors.whiteColor,
-                  fontSize: 18,
-                ),
-              ),
-              const SizedBox(height: 15,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextCustom(
-                    text: 'عندك حساب ؟',
-                    color: AppColors.gray800,
-                    fontSize: 18,
-                  ),
-                  TextButton(
-                      onPressed: (){
-                        Navigator.pop(context);
+    return BlocProvider(
+      create: (context) => AuthCubit(),
+      child: BlocBuilder<AuthCubit, AuthState>(
+        builder: (context, state) {
+          AuthCubit authCubit = AuthCubit.get(context);
+          return Directionality(
+            textDirection: TextDirection.rtl,
+            child: Scaffold(
+              body: Form(
+                key: authCubit.formKey,
+                child: ListView(
+                  physics: const BouncingScrollPhysics(),
+                  padding: const EdgeInsets.only(top: 150, right: 18, left: 18),
+                  children: [
+                    TextCustom(
+                      text: 'todo_app',
+                      fontSize: 25,
+                      color: AppColors.appColor.withOpacity(0.6),
+                      fontWeight: FontWeight.bold,
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    const TextCustom(
+                      text: 'إنشاء حساب',
+                      fontWeight: FontWeight.bold,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextFormFieldCustom(
+                      labelText: 'اسم المستخدم',
+                      keyboardType: TextInputType.text,
+                      controller: authCubit.nameController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'اسم المستخدم مطلوب';
+                        }
+                        return null;
                       },
-                      style: TextButton.styleFrom(
-                        splashFactory: NoSplash.splashFactory,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFormFieldCustom(
+                      labelText: 'البريد الالكترنى',
+                      keyboardType: TextInputType.emailAddress,
+                      controller: authCubit.emailController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'البريد الالكترونى مطلوب';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFormFieldCustom(
+                      labelText: ' كلمة المرور',
+                      keyboardType: TextInputType.text,
+                      controller: authCubit.passwordController,
+                      obscureText: true,
+                      icon: AppIcons.visibilityIcon,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'كلمة المرور مطلوبة';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFormFieldCustom(
+                      labelText: ' تاكيد كلمة المرور',
+                      keyboardType: TextInputType.text,
+                      controller: authCubit.passwordConfirmationController,
+                      obscureText: true,
+                      textInputAction: TextInputAction.done,
+                      icon: AppIcons.visibilityIcon,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'كلمة المرور مطلوبة';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    ElevatedButtonCustom(
+                      onPressed: () {
+                        if (authCubit.formKey.currentState!.validate()) {
+                          AuthCubit.get(context).register().then((value) {
+                            navigationPushFunction(
+                                context: context,
+                                screen: const TodoHomePageScreen());
+                          });
+                        }
+                      },
+                      child: Visibility(
+                        visible: state is RegisterLoadingState,
+                        replacement: const TextCustom(
+                          text: 'إنشاء حساب',
+                          color: AppColors.whiteColor,
+                          fontSize: 18,
+                        ),
+                        child: const CircularProgressLoadingCustom(),
                       ),
-                      child: const TextCustom(
-                        text: 'تسجيل دخول',
-                        fontSize: 18,
-                      )
-                  )
-                ],
-              )
-            ],
-          ),
-        ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextCustom(
+                          text: 'عندك حساب ؟',
+                          color: AppColors.gray800,
+                          fontSize: 18,
+                        ),
+                        TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            style: TextButton.styleFrom(
+                              splashFactory: NoSplash.splashFactory,
+                            ),
+                            child: const TextCustom(
+                              text: 'تسجيل دخول',
+                              fontSize: 18,
+                            ))
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
