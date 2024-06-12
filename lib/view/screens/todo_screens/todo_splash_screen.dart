8c35cd14358ea79ff3a08361cfc44d174922e482
Rehab Saad <saadrehab075@gmail.com>
2024-06-13 02:52:32 +0300

    edit next screen in splash screen

--- lib/view/screens/todo_screens/todo_splash_screen.dart
+++ lib/view/screens/todo_screens/todo_splash_screen.dart
@@ -2,6 +2,8 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/view/screens/auth_screens/login_screen.dart';
 import 'package:todo_app/view/screens/todo_screens/todo_home_page_screen.dart';
+import 'package:todo_app/view_model/data/local/shared_keys.dart';
+import 'package:todo_app/view_model/data/local/shared_preferences.dart';
 
 class TodoSplashScreen extends StatelessWidget {
   const TodoSplashScreen({super.key});
@@ -11,7 +13,7 @@ class TodoSplashScreen extends StatelessWidget {
     return  AnimatedSplashScreen(
         duration: 3000,
         splash: 'assets/images/todo_splash_image.jpg',
-        nextScreen: const LoginScreen(),
+        nextScreen:LocalData.get(key:SharedKeys.token)!=null?const TodoHomePageScreen():const LoginScreen(),
         splashIconSize: 300.0,
         splashTransition: SplashTransition.fadeTransition,
     );
